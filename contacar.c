746642ce64dca63d12264c708f848634483e6629
AntonioLegnante <AntonioLegnante12@gmail.com>
2019-03-03 14:57:47 +0000

    conteggio caratteri

---
+++ contacar.c
@@ -0,0 +1,18 @@
+/*contacaratteri */
+
+#include <stdio.h>
+
+int main ()
+{
+    int a, contatore = 0;
+
+    printf("conta i caratteri del file\n");
+    
+    while((a = getchar()) != EOF)
+        contatore++;
+
+    printf("%d", contatore);
+    
+    return 0;
+
+}

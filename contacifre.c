0272f2a92540fe21e9eff5c7137be7614058061a
AntonioLegnante <AntonioLegnante12@gmail.com>
2019-03-09 12:14:45 +0000

    trucco ascii

--- contacifre.c
+++ contacifre.c
@@ -10,51 +10,19 @@ int main ()
 
     while((a = getchar()) != EOF)
     {
-        switch(a)
+        if(a > 48 || a < 57)
         {
-            case '0':
-               contatorenum[0]++;
-            break;
-            case '1':
-               contatorenum[1]++;
-            break;
-            case '2':
-               contatorenum[2]++;
-            break;
-            case '3':
-               contatorenum[3]++;
-            break;
-            case '4':
-               contatorenum[4]++;
-            break;
-            case '5':
-               contatorenum[5]++;
-            break;
-            case '6':
-               contatorenum[6]++;
-            break;
-            case '7':
-               contatorenum[7]++;
-            break;
-            case '8':
-               contatorenum[8]++;
-            break;
-            case '9':
-               contatorenum[9]++;
-            break;
-            
-            case' ':
+            i = a - 48;
+            contatorenum[i]++;
+        }
+         else if(a == ' ')
             contatorespazi++;
-            break;
-
-            default:
+         else
             contatore++;
-            break;
 
-            }
     }
 
-    printf("sono presenti %3d caratteri,%3d sono gli spazi\n", contatore, contatorespazi);
+    printf("sono presenti %3d caratteri %3d sono gli spazi\n", contatore, contatorespazi);
     for(i = 0; i < 10; i++)
         printf("sono presenti %d %d\n", contatorenum[i], i);
 

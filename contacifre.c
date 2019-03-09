491ce17e8ac3a8346241f239e0021c1326764b7b
AntonioLegnante <AntonioLegnante12@gmail.com>
2019-03-09 03:45:17 +0000

    contacifre

--- contacifre.c
+++ contacifre.c
@@ -4,7 +4,7 @@
 
 int main ()
 {
-    int contatore = 0, a, contatorenum = 0, contatorespazi = 0;
+    int contatore = 0, a, contatorenum[10] = {0}, contatorespazi = 0, i;
 
     printf("conteggio delle righe\n");
 
@@ -12,17 +12,35 @@ int main ()
     {
         switch(a)
         {
-            case '1':
-            case '2':
-            case '3':
-            case '4':
-            case '5':
-            case '6':
-            case '7':
-            case '8':
             case '0':
+               contatorenum[0]++;
+            break;
+            case '1':
+               contatorenum[1]++;
+            break;
+            case '2':
+               contatorenum[2]++;
+            break;
+            case '3':
+               contatorenum[3]++;
+            break;
+            case '4':
+               contatorenum[4]++;
+            break;
+            case '5':
+               contatorenum[5]++;
+            break;
+            case '6':
+               contatorenum[6]++;
+            break;
+            case '7':
+               contatorenum[7]++;
+            break;
+            case '8':
+               contatorenum[8]++;
+            break;
             case '9':
-            contatorenum++;
+               contatorenum[9]++;
             break;
             
             case' ':
@@ -36,7 +54,9 @@ int main ()
             }
     }
 
-    printf("sono presenti %d caratteri, %d sono le cifre e %d sono gli spazi", contatore, contatorenum, contatorespazi);
+    printf("sono presenti %3d caratteri,%3d sono gli spazi\n", contatore, contatorespazi);
+    for(i = 0; i < 10; i++)
+        printf("sono presenti %d %d\n", contatorenum[i], i);
 
     return 0;
 

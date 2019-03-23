308cde7e007a00c3f1956e680f0dedb870b9f1c4
AntonioLegnante <AntonioLegnante12@gmail.com>
2019-03-23 12:53:04 +0100

    uso degli stati e di enum

--- contaparole.c
+++ contaparole.c
@@ -1,28 +1,35 @@
 /*conta parole*/
+/*stato NORMALE a == ' ' || a == '\n' || a == '\t'  contaparole++
+                                                    stato = CONTA
+                a == altri caratteri                NULL
+  stato CONTA   a == altri caratteri                stato = NORMALE */      
 
 #include <stdio.h>
 
 int main ()
 {
-  enum stato {normale, nuovostato};
+  enum stato {NORMALE, CONTA};
   int a, contatorerighe = 0, contatorecar = 0, contaparole = 0;
-  enum stato stato = normale;
+  enum stato stato = NORMALE;
 
+  /*enum stato, tipologia di variabile*/
+  
   while((a = getchar()) != EOF)
   {
       contatorecar++;
 
       if(a == '\n')    contatorerighe++;
 
-      if(stato == normale)
+      if(stato == NORMALE)
       {
-          if(a == ' ' || a == '\n' || a == '\t')    stato = nuovostato;
+          if(a == ' ' || a == '\n' || a == '\t')    
+          {
+              stato = CONTA;
+              contaparole++;
+          }
       }
-      if(stato == nuovostato)
-      {
-          contaparole++;
-          stato = normale;
-      }
+      else if(stato == CONTA)    stato = NORMALE;
+
           
   }
 

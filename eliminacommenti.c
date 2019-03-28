62bf09c85d844ad7612d0d5b8825ffae8a2f0b74
AntonioLegnante <AntonioLegnante12@gmail.com>
2019-03-28 14:45:07 +0100

    elimina commenti

--- eliminacommenti.c
+++ eliminacommenti.c
@@ -1,42 +1,113 @@
-/*elimina commenti*/
+/*elimina commenti*/    
+/* stato NORMALE           a = "               stato SCRORRI
+                                               stampa a
+                           a = /               stato ASTERISCO
+                           a = '               stato VIRGOLETTA
+                                               stampa a 
+                           a = altri caratteri stato stampa a
 
-#include <stdio.h>
+   stato SCORRI            a = "               stato NORMALE
+                         stampa a 
+                           a = altri caratteri stampa a
+
+   stato VIRGOLETTA
+                           a = /               stato STAMPA_VIRGOLETTA
+                                               stampa a
+                           a = *               stato NORMALE
+                                               stampa a
+                           a = altri caratteri stampa a
+
+   stato STAMPA_VIRGOLETTA a = '               stampa a
+                                               stato NORMALE
+
+   stato ASTERISCO         a = *               stato elimina
+                           a = altri caratteri stampa /
+                                               stampa a 
+                                               stato NORMALE
+   stato ELIMINA           a = *               stato FINECOMMENTO
+
+   stato FINECOMMENTO      a = /               stato = NORMALE
+                           a = altri caratteri stato = ELIMINA*/
+#include <stdio.h> 
 
 int main ()
 {
-  enum stato {normale, nuovostato, eliminacommento, eliminacommento1};
+  enum stato {NORMALE, ASTERISCO, ELIMINA, FINECOMMENTO, SCORRI, VIRGOLETTA, STAMPA_VIRGOLETTA};
   char a;
-  enum stato stato = normale;
+  enum stato stato = NORMALE;
 
   while((a = getchar()) != EOF)
   {
-      if(stato == normale)
+      if(stato == NORMALE)
       {
-          if(a == '/')  stato = nuovostato;
+          if(a == '"')  
+          {
+              stato = SCORRI;
+              putchar(a);
+          }
+          else if(a == '/')  stato = ASTERISCO;
+          else if(a == '\'')  
+          {
+              stato = VIRGOLETTA;
+              putchar(a);
+          }
+          else   putchar(a);
+      }
+     
+      else if(stato == SCORRI)
+      {
+          if(a == '"')  
+          {
+              stato = NORMALE;
+              putchar(a);
+          }
           else    putchar(a);
       }
-      else if(stato == nuovostato)
+      else if(stato == VIRGOLETTA)
+      { 
+          if(a == '/')
+          {
+              stato = STAMPA_VIRGOLETTA;
+              putchar(a);
+          }
+          else if(a == '*')
+          {
+              stato = NORMALE;
+              putchar(a);
+          }
+          else    putchar(a);
+      } 
+      else if(stato == STAMPA_VIRGOLETTA)
+      {
+          if(a == '\'')
+          {
+              putchar(a);
+              stato = NORMALE;
+          } 
+      }
+      else if(stato == ASTERISCO)
       {    
-          if(a == '*')  stato = eliminacommento;
+          if(a == '*')  stato = ELIMINA;
           else    
           {
               putchar('/');
               putchar(a);
-              stato = normale;
+              stato = NORMALE;
           }
       }
 
-      else if(stato == eliminacommento)
+      else if(stato == ELIMINA)
       {
-          if(a == '*')    stato = eliminacommento1;
+          if(a == '*')    stato = FINECOMMENTO;
       }
+ 
+      else if(stato == FINECOMMENTO)
+      {
+          if(a == '/')   stato = NORMALE;
+          else    stato = ELIMINA;
+      }
+    
 
-      else if(stato == eliminacommento1)
-      {
-          if(a == '/')   stato = normale;
-      }
-      
-          
   }
   return 0;
 }
